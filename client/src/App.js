5054257932f4940f9cfc42b14017700384ba6f9c
Kristina Brennan <96805863+thetinaest@users.noreply.github.com>
2022-09-03 21:50:15 -0500

    set up concurrently

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,7 @@ import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@ap
 
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:3001/graphql',
+  uri: '/graphql',
 });
 
 const client = new ApolloClient({
--- server/server.js
+++ server/server.js
@@ -21,6 +21,15 @@ app.use(express.json());
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
   server.applyMiddleware({ app });
+
+  // Serve up static assets
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+  }
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
   
   db.once('open', () => {
     app.listen(PORT, () => {
